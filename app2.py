a9f38484d67501bbe1fcbd15f7b89549c5472dfa
huckjoo <huckjoo96@naver.com>
2021-09-29 17:17:26 +0900

    html extends

--- app2.py
+++ app2.py
@@ -1,40 +1,113 @@
-from bson.objectid import ObjectId
-from flask import Flask, render_template, request
-from flask import redirect, url_for
-from pymongo import MongoClient
-from datetime import datetime
-import time
+from main import *
 
-client = MongoClient("localhost", 27017)
-db = client.dbapp2
 
-from flask import Flask
+@app.route("/comment_list/<root_id>")
+def comment_list(root_id):
+    comments = db.comments.find({"root_id": str(root_id)}).sort("pubdate", -1)
+    comment_list = []
+    for c in comments:
+        comment = {
+            "id": str(c.get("_id")),
+            "name": c.get("name"),
+            "comment": c.get("comment"),
+            "root_id": c.get("root_id"),
+            "pubdate": filter.format_datetime(c.get("pubdate")),
+        }
+        comment_list.append(comment)
 
-app = Flask(__name__)
+    return jsonify(response="success", lists=comment_list)
 
 
 @app.route("/list")
 def board_lists():
-    datas = db.board.find({})
-    return render_template("list2.html", datas=datas)
 
+    # 페이지 값(default=1)
+    page = request.args.get("page", 1, type=int)
+    # 한페이지당 몇개의 게시물을 출력할지
+    limit = request.args.get("limit", 7, type=int)
+    datas = db.board.find({}).skip((page - 1) * limit).limit(limit).sort("pubdate", -1)
+    # 게시물의 총 개수
+    tot_count = db.board.find({}).count()
+    # 마지막 페이지의 수 구하기
+    last_page_num = math.ceil(tot_count / limit)
+    # 페이지 블럭 5개씩 표기
+    block_size = 5
+    # 현재 블럭 위치
+    block_num = int((page - 1) / block_size)
+    # 블럭의 시작 위치
+    block_start = int((block_size * block_num) + 1)
+    # 블럭의 끝 위치
+    block_last = math.ceil(block_start + (block_size - 1))
 
-@app.template_filter("formatdatetime")
-def format_datetime(value):
-    if value is None:
-        return ""
-    now_timestamp = time.time()
-    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(
-        now_timestamp
+    return render_template(
+        "list2.html",
+        datas=datas,
+        limit=limit,
+        page=page,
+        block_start=block_start,
+        block_last=block_last,
+        last_page_num=last_page_num,
     )
-    value = datetime.fromtimestamp((int(value) / 1000)) + offset
-    return value.strftime("%Y-%m-%d %H:%M:%S")
+
+
+@app.route("/comments", methods=["POST"])
+def board_comments():
+    name = request.form["name"]
+    comment = request.form["comment"]
+    root_id = request.form["root_id"]
+    current_utc_time = round(datetime.utcnow().timestamp() * 1000)
+    post = {
+        "name": name,
+        "comment": comment,
+        "root_id": str(root_id),
+        "pubdate": current_utc_time,
+    }
+    db.comments.insert_one(post)
+    return redirect(url_for("board_view", idx=root_id))
+
+
+@app.route("/delete/<idx>", methods=["GET", "POST"])
+def delete(idx):
+    db.board.delete_one({"_id": ObjectId(idx)})
+    return redirect(url_for("board_lists", idx=idx))
+
+
+@app.route("/edit/<idx>", methods=["GET", "POST"])
+def edit(idx):
+    if request.method == "GET":
+        data = db.board.find_one({"_id": ObjectId(idx)})
+        return render_template("edit2.html", data=data)
+    else:
+        name = request.form["name"]
+        title = request.form["title"]
+        contents = request.form["contents"]
+        db.board.update_one(
+            {"_id": ObjectId(idx)},
+            {"$set": {"name": name, "title": title, "contents": contents}},
+        )
+        return redirect(url_for("board_view", idx=idx))
 
 
 @app.route("/view/<idx>", methods=["GET", "POST"])
 def board_view(idx):
-    data = db.board.find_one({"_id": ObjectId(idx)})
-    return render_template("view2.html", data=data)
+    if request.method == "GET":
+        page = request.args.get("page")
+        # data = db.board.find_one({"_id": ObjectId(idx)})
+        data = db.board.find_one_and_update(
+            {"_id": ObjectId(idx)},
+            {"$inc": {"view": 1}},
+            return_document=True,
+        )  # return_document=True는 적용된 후에 data를 받는것을 의미
+        if data is not None:
+            result = {
+                "id": data.get("_id"),
+                "name": data.get("name"),
+                "title": data.get("title"),
+                "contents": data.get("contents"),
+                "pubdate": data.get("pubdate"),
+                "view": data.get("view"),
+            }
+        return render_template("view2.html", result=result, page=page)
 
 
 @app.route("/write", methods=["GET", "POST"])
